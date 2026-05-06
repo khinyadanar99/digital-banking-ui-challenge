42ce991b880ec13fb8b0e06b998da5606d1f9f4a
maythinn <developer.maythinn@gmail.com>
2026-05-06 13:48:32 +0630

    fix: not finish mobile hero

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -8,24 +8,26 @@ function HeroSection() {
 
   return (
     <div className=" min-h-[80vh]">
-      <Section>
-        <div className="flex">
+      <Section customClass={"px-0"}>
+        <div className="flex flex-col-reverse">
           <div className="">
-            <div className="w-md py-40">
+            <div className="md:w-md py-40">
               <h1 className="text-6xl heading-color">{hero.title}</h1>
               <p className="paragraph-color py-5">{hero.subtitle}</p>
               <Button></Button>
             </div>
           </div>
-          <div className="h-[50vh] bg-red-500 w-screen">sss</div>
-          <div className="absolute -top-45 -right-110">
-            <img src="/images/bg-intro-desktop.svg" alt="img" />
+          <div className="w-screen h-[50vh] bg-amber-200">
+            <div className="absolute md:-top-45 md:-right-110">
+              <img src="/images/bg-intro-desktop.svg" className="hidden md:block" alt="img" />
+                <img src="/images/bg-intro-mobile.svg" alt="img" className="md:hidden" />
+            </div>
+            {/* <img
+              className="absolute z-40 -top-20 -right-30"
+              src="/images/image-mockups.png"
+              alt="img"
+            /> */}
           </div>
-          <img
-            className="absolute z-40 -top-20 -right-30"
-            src="/images/image-mockups.png"
-            alt="img"
-          />
         </div>
       </Section>
     </div>
